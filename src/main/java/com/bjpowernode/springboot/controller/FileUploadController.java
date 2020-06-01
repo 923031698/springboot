c489e9f860af1b58545625e0d5e1895cd2266354
小小小柏 <923031698@qq.com>
2020-06-01 16:47:00 +0800

    新写一个文件上传接口

---
+++ src/main/java/com/bjpowernode/springboot/controller/FileUploadController.java
@@ -0,0 +1,26 @@
+package com.bjpowernode.springboot.controller;
+
+import com.bjpowernode.springboot.common.utils.ResponseUtils;
+import com.bjpowernode.springboot.model.common.Response;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+
+/**
+ * 文件上传
+ */
+@RestController
+public class FileUploadController {
+    /**
+     * 处理文件上传
+     */
+    @RequestMapping("/fileUploadController")
+    public Response fileUpload(MultipartFile filename) throws Exception {
+        System.out.println(filename.getOriginalFilename());
+        //打印文件上传名称
+        filename.transferTo(new File("D:/" + filename.getOriginalFilename()));//文件保存
+        return ResponseUtils.success();
+    }
+}
